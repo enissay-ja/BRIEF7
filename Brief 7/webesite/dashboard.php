ea1f9cd793357b673ae858d80cf4e82dc5f6e5cb
enissay-ja <78980526+enissay-ja@users.noreply.github.com>
2023-01-22 18:01:01 +0100

    Add files via upload

---
+++ Brief 7/webesite/dashboard.php
@@ -0,0 +1,181 @@
+<?php
+include_once("header.php");
+?>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+      <link rel="stylesheet" href="http://localhost/Hotel/public/css/dash.css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <title>Dashboard</title>
+  </head>
+
+  <body>
+      <div class="d-flex" id="wrapper">
+          <!-- Sidebar -->
+          <div class="bg-white" id="sidebar-wrapper">
+              <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><img src="logo" alt=""></div>
+              <div class="list-group list-group-flush my-3">
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active"><i
+                          class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-project-diagram me-2"></i>Projects</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-chart-line me-2"></i>Analytics</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-paperclip me-2"></i>Reports</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-shopping-cart me-2"></i>Store Mng</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-gift me-2"></i>Products</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-comment-dots me-2"></i>Chat</a>
+                  <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
+                          class="fas fa-map-marker-alt me-2"></i>Outlet</a>
+                  <a href="http://localhost/Hotel/public/User/out" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i
+                          class="fas fa-power-off me-2"></i>Logout</a>
+              </div>
+          </div>
+          <!-- /#sidebar-wrapper -->
+
+          <!-- Page Content -->
+          <div id="page-content-wrapper">
+              <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+                  <div class="d-flex align-items-center">
+                      <i class="fas fa-align-left icon-back fs-4 me-3" id="menu-toggle"></i>
+                      <h2 class="fs-2 m-0">Dashboard</h2>
+                  </div>
+
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                      aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
+
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                          <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
+                                  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                  <i class="fas fa-user me-2"></i><?php if(isset($_SESSION['name'])){
+                                      echo $_SESSION['name']; 
+                                      } ?>
+                              </a>
+                              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                  <!-- <li><a class="dropdown-item" href="#">Profile</a></li> -->
+                                  <li><a class="dropdown-item" href="#"><?php if(isset($_SESSION['email'])){
+                                      echo $_SESSION['email']; 
+                                      } ?></a></li>
+                                  <li><a class="dropdown-item" href="#">Settings</a></li>
+                                  <li><a class="dropdown-item" href="#">Logout</a></li>
+                              </ul>
+                          </li>
+                      </ul>
+                  </div>
+              </nav>
+              <div class="container-fluid px-4">
+                  <div class="row g-3 my-2">
+                      <div class="col-md-3">
+                          <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                              <div>
+                                  <h3 class="fs-2">720</h3>
+                                  <p class="fs-5">Room</p>
+                              </div>
+                              <i class="fas fa-gift fs-1  border rounded-full secondary-bg p-3"></i>
+                          </div>
+                      </div>
+
+                      <div class="col-md-3">
+                          <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                              <div>
+                                  <h3 class="fs-2">4920</h3>
+                                  <p class="fs-5">Sales</p>
+                              </div>
+                              <i
+                                  class="fas fa-hand-holding-usd fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                          </div>
+                      </div>
+
+                      <div class="col-md-3">
+                          <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                              <div>
+                                  <h3 class="fs-2">3899</h3>
+                                  <p class="fs-5">Delivery</p>
+                              </div>
+                              <i class="fas fa-truck fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                          </div>
+                      </div>
+
+                      <div class="col-md-3">
+                          <div class="p-3 bg-white shadow-sm d-flex justify-content-around align-items-center rounded">
+                              <div>
+                                  <h3 class="fs-2">%25</h3>
+                                  <p class="fs-5">Increase</p>
+                              </div>
+                              <i class="fas fa-chart-line fs-1 primary-text border rounded-full secondary-bg p-3"></i>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="row my-5">
+                      <div class="d-flex justify-content-between align-items-center" >
+                          <h3 class="fs-4 mb-3">Rooms</h3>
+                          <a href="http://localhost/Hotel/public/Admin/add" class="text-light btn btn-secondary ">Add New Room</a>
+                      </div>
+                      <div class="col">
+                          <table class="table bg-white rounded shadow-sm  table-hover">
+                              <thead>
+                                  <tr>
+                                      <th scope="col" width="50">#</th>
+                                      <th scope="col">Image</th>
+                                      <th scope="col">Type</th>
+                                      <th scope="col">Suite type</th>
+                                      <th scope="col">Capacity</th>
+                                      <th scope="col">Price</th>
+                                      <th scope="col">Description</th>
+                                      <th scope="col">Action</th>
+                                  </tr>
+                              </thead>
+                              <tbody>
+                              <?php foreach ($data as $room) { ?>
+                                  <tr>
+                                      <th scope="row">1</th>
+                                      <td><img src="http://localhost/Hotel/public/Uppictures/<?php echo  $room['image'] ;?>" alt="" srcset="" width="50px"></td>
+                                      <td><?php echo $room['type'] ;?></td>
+                                      <td><?php echo $room['suite_type'] ;?></td>
+                                      <td><?php echo $room['capacity'] ;?></td>
+                                      <td><?php echo $room['price' ] ;?></td>
+                                      <td><?php echo $room['description'] ;?></td>
+                                      <td>
+                                      <a href="http://localhost/Hotel/public/Admin/up/<?php echo  $room['id'] ; ?>" style="text-decoration:none;" ><i class="fa-solid fa-pen text-warning"></i> </a>
+                                      <a href="http://localhost/Hotel/public/Admin/delete/<?php echo  $room['id'] ; ?>" style="text-decoration:none;" > <i class="fa-solid fa-trash text-danger"></i> </a>
+                                      </td>
+                                  </tr>
+                              <?php };?> 
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <!-- /#page-content-wrapper -->
+      </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+      <script>
+          var el = document.getElementById("wrapper");
+          var toggleButton = document.getElementById("menu-toggle");
+
+          toggleButton.onclick = function () {
+              el.classList.toggle("toggled");
+          };
+      </script>
+  </body>
+
+  </html>
---
+++ Brief 7/webesite/shop.php
@@ -0,0 +1,310 @@
+<?php
+include_once("header.php");
+?>
+
+
+<section class="section-products">
+		<div class="container">
+				<div class="row justify-content-center text-center">
+						<div class="col-md-8 col-lg-6">
+								<div class="header">
+										<h2 class="font-weight-bold">List of all products</h2>
+								</div>
+						</div>
+				</div>
+				<div class="row">
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-1" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-2" class="single-product">
+										<div class="part-1">
+												<span class="discount">15% off</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-3" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-4" class="single-product">
+										<div class="part-1">
+												<span class="new">new</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-5" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-6" class="single-product">
+										<div class="part-1">
+												<span class="discount">15% off</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-7" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-8" class="single-product">
+										<div class="part-1">
+												<span class="new">new</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+                        <!-- Single Product -->
+                        <div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-1" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-2" class="single-product">
+										<div class="part-1">
+												<span class="discount">15% off</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-3" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-4" class="single-product">
+										<div class="part-1">
+												<span class="new">new</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-5" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-6" class="single-product">
+										<div class="part-1">
+												<span class="discount">15% off</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-7" class="single-product">
+										<div class="part-1">
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+						<!-- Single Product -->
+						<div class="col-md-6 col-lg-4 col-xl-3">
+								<div id="product-8" class="single-product">
+										<div class="part-1">
+												<span class="new">new</span>
+												<ul>
+														<li><a><i class="fas fa-shopping-cart"></i></a></li>
+														<li><a><i class="fas fa-heart"></i></a></li>
+														<li><a><i class="fas fa-plus"></i></a></li>
+														<li><a><i class="fas fa-expand"></i></a></li>
+												</ul>
+										</div>
+										<div class="part-2">
+												<h3 class="product-title">Product Name</h3>
+												<h4 class="product-old-price">$79.99</h4>
+												<h4 class="product-price">$49.99</h4>
+										</div>
+								</div>
+						</div>
+				</div>
+		</div>
+</section>
+
+<?php
+include_once("footer.php");
+?>
